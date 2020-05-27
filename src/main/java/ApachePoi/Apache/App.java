6e5a8fb40b4d608259f30403aba9afd73d01f6e0
Nisha9597 <nishait9597@gmail.com>
2020-05-27 15:50:34 +0530

    first git

---
+++ src/main/java/ApachePoi/Apache/App.java
@@ -0,0 +1,13 @@
+package ApachePoi.Apache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/testData/constant.java
@@ -0,0 +1,23 @@
+package testData;
+
+public class constant {
+	public static final String URL = "https://parabank.parasoft.com/parabank/register.htm";
+	
+	public static final String FirstName= "nishaj";
+	public static final String LastName = "j";
+	public static final String  Address= "chennai";
+	public static final String city = "chennai";
+    public static final String state = "Tamilnadu";
+    public static final String Zipcode= "600027";
+    public static final String Phone = "12345678";
+    public static final String SSN = "12345";
+    public static final String username= "nishaj";
+	public static final String password = "nishaj123";
+	public static final String confirmpassword= "nishaj123";
+    
+    
+    
+    public static final String Path_TestData = "C:\\Users\\hp\\maven\\Apache\\src\\main\\java\\testData";
+
+    public static final String File_TestData = "filosheet.xlsx";
+}
